901d85332dad9fcf8d4711ffd310ec7eadca6ed8
Veronika <124172108+dudosinka0@users.noreply.github.com>
2026-03-17 21:59:38 +0300

    first

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _prizePrefab;
     [SerializeField] private float _speed = 5f;
 
     private Vector3 _targetPosition;
@@ -14,6 +15,12 @@ public class PlayerMovements : MonoBehaviour
         _player = Instantiate(_player);
         _player.transform.position = new Vector3(0, 0, 0);
         _targetPosition = _player.transform.position;
+
+        for (int i = 0; i < 6; i++)
+        {
+            var prize = Instantiate(_prizePrefab);
+            prize.transform.position = new Vector3(Random.Range(-9f, 9f), 0f, Random.Range(-9f, 9f));
+        }
     }
 
     private void Update()
@@ -30,6 +37,8 @@ public class PlayerMovements : MonoBehaviour
                 _targetPosition = ray.GetPoint(distance);
             }
         }
+
+        _player.transform.LookAt(_targetPosition);
 
         // Двигаемся к цели
         _player.transform.position = Vector3.MoveTowards(
